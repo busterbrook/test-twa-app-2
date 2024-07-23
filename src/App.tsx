8ce17fb236b49af8a2e34b770f36fbd0ada72370
Alexander Yakunin <51092585+busterbrook@users.noreply.github.com>
2024-07-23 21:43:56 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { TonConnectButton } from '@tonconnect/ui-react'
 import './App.css'
 import '@twa-dev/sdk'
 
 function App() {
   return (
     <>
+    <div>
+      <TonConnectButton/>
+    </div>
     <div>
       <h1> Test APP </h1>
       <p> Sync You </p>
