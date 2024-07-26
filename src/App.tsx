b7f4bde0921793171b23fd9579f4c5e2182db320
Alexander Yakunin <51092585+busterbrook@users.noreply.github.com>
2024-07-26 09:18:10 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
+import {useState} from "react";
 import { TonConnectButton } from '@tonconnect/ui-react'
-import {useSendContract} from "./hooks/useLightSendContract"
+//import {useSendContract} from "./hooks/useLightSendContract"
 import './App.css'
 import '@twa-dev/sdk'
+import react from "@vitejs/plugin-react";
 
 function App() {
+  const [amountOut] = useState("0");
+  const [amountIn] = useState("0");
+  const [currencyOut] = useState("");
+  const [currencyIn] = useState("");
+
+  const writeData = () => {
+    console.log(amountOut, amountIn, currencyOut, currencyIn);
+  }
+
+  const readData = () => {
+    console.log("read from DB");
+  }
+
   return (
-    <>
-    <div>
-      <TonConnectButton/>
-    </div>
-    <div>
-      <h1> Test APP </h1>
-      <p> Sync You </p>
-    </div>
-    <div>
-      <button
-      onClick={useSendContract}>
-        SEND
-      </button>
-    </div>
-    </>
+      <>
+        <div className='Application'>
+          <div className='Header'>
+            <TonConnectButton/>
+            <button>
+              LK
+            </button>
+          </div>
+
+          <div className='Label'>
+            <img src=''/>
+            <h1> OURO </h1>
+          </div>
+
+          <div className='Main'>
+            <div className='InputFrom'>
+              <p>Ton Out</p>
+              <input
+                  type="int"
+                  value={amountOut}
+                  //onChange={onChange}
+              />
+              <p>Currency Out</p>
+              <input
+                  type="int"
+                  value={amountOut}
+                  //onChange={onChange}
+              />
+              <p>Ton In</p>
+              <input
+                  type="int"
+                  value={amountOut}
+                  //onChange={onChange}
+              />
+              <p>Currency In</p>
+              <input
+                  type="int"
+                  value={amountOut}
+                  //onChange={onChange}
+              />
+            </div>
+
+            <button
+                onClick={writeData}>
+              Create Offer
+            </button>
+          </div>
+
+          <div>
+            <button
+                onClick={readData}>
+              Swap
+            </button>
+          </div>
+
+          <div className='Footer'>
+            <p>Developed by Anon's members</p>
+          </div>
+        </div>
+      </>
   )
 }
 
