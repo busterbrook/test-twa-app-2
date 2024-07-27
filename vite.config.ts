6f10d7d559cd9f41ee4634a741c67de26d2fb3ea
Alexander Yakunin <51092585+busterbrook@users.noreply.github.com>
2024-07-27 23:13:52 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 /*
@@ -25,6 +26,11 @@ export default defineConfig({
         })
       ]
     }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer/', 'Buffer'] })],
+    },
   },
   base: "/test-twa-app-2/"
 })
